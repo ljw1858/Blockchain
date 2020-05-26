3da2004377681bb006f6c8da783cdac6d2f904c9
ljw1858 <62677956+ljw1858@users.noreply.github.com>
2020-05-26 17:25:52 +0900

    pq
    pq

--- blockchain.py
+++ blockchain.py
@@ -21,6 +21,7 @@ from Crypto.PublicKey import RSA  # for rsa verification
 from base64 import b64encode, b64decode  # for rsa verification (string public key to public key object)
 from multiprocessing import Process, Lock  # for using Lock method(acquire(), release())
 # 체크 확인용 (0526)
+# check pull Reqeuest (성훈) 0526
 # for Put Lock objects into variables(lock)
 lock = Lock()
 
