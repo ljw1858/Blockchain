4d0c6c4385c7d901ca56e5837f5e69dc7c4a66da
ljw1858 <62677956+ljw1858@users.noreply.github.com>
2020-05-26 13:56:24 +0900

    tt
    tt

--- blockchain.py
+++ blockchain.py
@@ -20,6 +20,7 @@ from sqlalchemy import create_engine  # for database connection
 from Crypto.PublicKey import RSA  # for rsa verification
 from base64 import b64encode, b64decode  # for rsa verification (string public key to public key object)
 from multiprocessing import Process, Lock  # for using Lock method(acquire(), release())
+# 0526 확인용 (성훈)
 
 # for Put Lock objects into variables(lock)
 lock = Lock()
